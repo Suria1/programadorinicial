cea11fc0bc62e172c488a89de331681c16d6e332
Suria <suriaespindola.com>
2024-07-22 23:14:58 -0300

    controladores

--- Modulo 3 - Unidad 2/app.js
+++ Modulo 3 - Unidad 2/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var serviciosRouter = require ('./routes/servicios');
+var contactoRouter = require ('./routes/contacto')
+
 var app = express();
 
 // view engine setup
@@ -21,14 +24,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/servicios', serviciosRouter);
+app.use('/contacto', contactoRouter);
+
+app.get('/prueba', function (req, res) {
+  res.send('Hola, es una pagina de prueba.')
+})
+
+app.get('/nosotros', function (req, res) {
+  res.send('Hola, es una pagina de nosotros.')
+})
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
