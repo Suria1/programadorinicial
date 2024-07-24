d3c351217d5689ba407f3009beb4f254026ad226
Suria <suriaespindola.com>
2024-07-23 22:41:41 -0300

    configuración de formulario

--- Sitio_UTN/routes/index.js
+++ Sitio_UTN/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+var nodemailer = require('nodemailer')
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index');
 });
 
-module.exports = router;
+router.post('/', async(req, res, next) => {
+  var email = req.body.email;
+  var nombre = req.body.nombre;
+  var ciudad = req.body.ciudad;
+
+  console.log(req.body)
+
+  var obj = {
+    to: 'suriaespindola@gmail.com',
+    subject: 'Contacto formulario',
+    html: nombre + email + " se contacta desde la ciudad: " + ciudad
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+  var info = await transport.sendMail(obj);
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente',
+  });
+});
+
+  module.exports = router;
