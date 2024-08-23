a914f8b1252a204c884dcf40c7fe3a6b46914db5
Suria <suriaespindola.com>
2024-08-23 02:54:08 -0300

    novedades/agregar/modificar

--- Sitio_UTN/modelos/novedadesModel.js
+++ Sitio_UTN/modelos/novedadesModel.js
@@ -1,11 +1,46 @@
 var pool = require('./base_de_datos');
 
 async function getNovedades() {
-    
+
         var query = 'select * from novedades';
         var rows = await pool.query(query);
         return rows;
-    
+
 }
 
-module.exports = { getNovedades }
+async function deleteNovedadesById(id) {
+        var query = 'delete from novedades where id = ?';
+        var rows = await pool.query(query, [id]);
+        return rows;
+}
+
+async function insertNovedades(obj) {
+        try {
+                var query = "insert into novedades set ?";
+                var rows = await pool.query(query, [obj])
+                return rows;
+        } catch (error) {
+                console.log(error);
+                throw error;
+        }
+}
+
+async function getNovedadbyId(id) {
+
+        var query = 'select * from novedades where id=?';
+        var rows = await pool.query(query, [id]);
+        return rows[0];
+
+}
+
+async function modificarNovedadById(obj, id) {
+        try {
+                var query = "update novedades set ? where id=? ";
+                var rows = await pool.query(query, [obj, id]);
+                return rows;
+        } catch (error) {
+                throw error;
+        }
+}
+
+module.exports = { getNovedades, deleteNovedadesById, insertNovedades, getNovedadbyId, modificarNovedadById }
