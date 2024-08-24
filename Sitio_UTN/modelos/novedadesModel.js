ed8805d690e95c32c3d42cceb9c561979ffea3f9
Suria <suriaespindola.com>
2024-08-23 21:12:55 -0300

    handlebars

--- Sitio_UTN/modelos/novedadesModel.js
+++ Sitio_UTN/modelos/novedadesModel.js
@@ -17,7 +17,7 @@ async function deleteNovedadesById(id) {
 async function insertNovedades(obj) {
         try {
                 var query = "insert into novedades set ?";
-                var rows = await pool.query(query, [obj])
+                var rows = await pool.query(query, [obj]);
                 return rows;
         } catch (error) {
                 console.log(error);
@@ -26,7 +26,7 @@ async function insertNovedades(obj) {
 }
 
 async function getNovedadById(id) {
-        var query = 'select * from novedades where id=?';
+        var query = "select * from novedades where id = ?";
         var rows = await pool.query(query, [id]);
         return rows[0];
 
@@ -34,7 +34,7 @@ async function getNovedadById(id) {
 
 async function modificarNovedadById(obj, id) {
         try {
-                var query = "update novedades set ? where id=? ";
+                var query = "update novedades set ? where id = ? ";
                 var rows = await pool.query(query, [obj, id]);
                 return rows;
         } catch (error) {
